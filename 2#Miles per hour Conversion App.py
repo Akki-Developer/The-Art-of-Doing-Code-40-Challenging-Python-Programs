dc2ac3091467f9cc515776a65f0c17c04467c60c
Akki-Developer <akanksharaj.it@gmail.com>
2020-08-12 13:31:06 +0530

    1st and 2nd Challenge updated

---
+++ 2#Miles per hour Conversion App.py
@@ -0,0 +1,4 @@
+print("Welcome to the MPS Conversion App")
+num = float(input("\nWhat is your speed in miles per hour: "))
+count_res=round(num*0.4474, 2)
+print("your speed in meters per second is "+str(count_res))
